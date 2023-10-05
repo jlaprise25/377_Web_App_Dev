46a7a1f358b0d717ff338e14ddcc77c21f6bd00b
Jeremy Laprise <jlaprise25@hanoverstudents.org>
2023-10-04 22:37:23 -0400

    Final

--- 2-equation-calculator/scripts.js
+++ 2-equation-calculator/scripts.js
@@ -1,6 +1,6 @@
 function calculatepH(){
     var concentration = document.getElementById('concentration').value;
-    var sigFigs = document.getElementById('sigfig').value;
+    var sigFigs = document.getElementById('pHsigfig').value;
 
     var pH = 0 - Math.log10(concentration);
 
@@ -18,7 +18,19 @@ function calculatepH(){
     document.getElementById("pH").innerHTML = pH;
 }
 
-function idealGas(){
-    var unknown = document.getElementById('variable').value;
-    
+function specificHeat(){
+    var m = document.getElementById('mass').value;
+    var c = document.getElementById('heatCapacity').value;
+    var tI = document.getElementById('ti').value;
+    var tF = document.getElementById('tf').value;
+
+    var dT = tF - tI;
+    var heat = m*c*dT;
+    document.getElementById("heat").innerHTML = heat;
+}
+
+function convertTemp(){
+    var c = document.getElementById('celcius');
+    var k = c + 273.15;
+    document.getElementById("kelvin").innerHTML = k;
 }
