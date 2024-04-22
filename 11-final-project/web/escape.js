19439bde2e5da77758f2202867051a043cc8970f
Jeremy Laprise <jlaprise25@hanoverstudents.org>
2024-04-22 10:57:18 -0400

    Milestone 3

---
+++ 11-final-project/web/escape.js
@@ -0,0 +1,12 @@
+function openDoor(code){
+    userCode = prompt("What is the Code?")
+    if(userCode == code){
+        alert("Thats the correct code")
+    } else {
+        alert("Thats incorrect")
+    }
+}
+
+function makeSodoku(){
+    var svgTest = document.createElement()
+}
